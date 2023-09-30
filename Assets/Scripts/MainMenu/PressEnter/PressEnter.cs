1f6f18dcc39a85a7530ac902f4164bb59acf48c8
TheSund <bormotonmatveyev@yandex.ru>
2023-09-30 20:31:53 +0300

    PressEnter Screen!!1!

---
+++ Assets/Scripts/MainMenu/PressEnter/PressEnter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.IsolatedStorage;
+using UnityEngine.UI;
+
+public class PressEnter : MonoBehaviour {
+
+	public bool ready;
+	public Canvas maincanvas;
+	public Canvas levelChanger;
+
+	// Use this for initialization
+	void Start()
+	{
+		Cursor.visible = false;
+		ready = false;
+		StartCoroutine(PressEnterBecomeReady());
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Return) && ready)
+		{
+			GetComponent<Animator>().Play("closing");
+			StartCoroutine(DisablePressEnterScreen());
+        }
+	}
+
+    private IEnumerator PressEnterBecomeReady()
+	{
+		yield return new WaitForSeconds(4.1f);
+		ready = true;
+	}
+
+	private IEnumerator DisablePressEnterScreen()
+	{
+		yield return new WaitForSeconds(1.3f);
+        maincanvas.gameObject.SetActive(true);
+        levelChanger.gameObject.SetActive(true);
+        Cursor.visible = true;
+        gameObject.SetActive(false);
+    }
+
+
+}
